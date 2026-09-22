fa04208493aa227c78149a0467088e44bfacc763
Ibrahimatatri <ibrahimatatri@gmail.com>
2026-09-22 10:27:58 -0400

    Return subtotal instead of taxed total

--- checkout.py
+++ checkout.py
@@ -1,6 +1,6 @@
 def calculate_total(price, quantity, tax_rate=0.06):
     subtotal = price * quantity
     total = subtotal * (1 + tax_rate)
-    return total
+    return subtotal
 
 print(calculate_total(10, 3))
